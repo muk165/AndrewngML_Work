2df1f0d06a3ea34e4e04a9e41373c66b63bdd978
muk165 <mukteshg156@gmai.com>
2022-08-28 14:35:42 +0530

    multiple linear regression and refrence to vectorization scripts

---
+++ venv/Vectorization.py
@@ -0,0 +1,81 @@
+import numpy as np    # it is an unofficial standard to use np for numpy
+import time
+
+# NumPy routines which allocate memory and fill arrays with value
+a = np.zeros(4);                print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.zeros((4,));             print(f"np.zeros(4,) :  a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.random.random_sample(4);
+print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+
+# NumPy routines which allocate memory and fill arrays with value but do not accept shape as input argument
+a = np.arange(4.);              print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.random.rand(4);
+print(f"np.random.rand(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+
+#vector indexing operations on 1-D vectors
+a = np.arange(10)
+print(a)
+
+#access an element
+print(f"a[2].shape: {a[2].shape} a[2]  = {a[2]}, Accessing an element returns a scalar")
+
+# access the last element, negative indexes count from the end
+print(f"a[-1] = {a[-1]}")
+
+#indexs must be within the range of the vector or they will produce and error
+try:
+    c = a[10]
+except Exception as e:
+    print("The error message you'll see is:")
+    print(e)
+
+
+
+#vector slicing operations
+a = np.arange(10)
+print(f"a         = {a}")
+
+#access 5 consecutive elements (start:stop:step)
+c = a[2:7:1];     print("a[2:7:1] = ", c)
+
+# access 3 elements separated by two
+c = a[2:7:2];     print("a[2:7:2] = ", c)
+
+# access all elements index 3 and above
+c = a[3:];        print("a[3:]    = ", c)
+
+# access all elements below index 3
+c = a[:3];        print("a[:3]    = ", c)
+
+# access all elements
+c = a[:];         print("a[:]     = ", c)
+
+
+
+np.random.seed(1)
+a = np.random.rand(10000000)  # very large arrays
+b = np.random.rand(10000000)
+
+tic = time.time()  # capture start time
+c = np.dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"np.dot(a, b) =  {c:.4f}")
+print(f"Vectorized version duration: {1000*(toc-tic):.4f} ms ")
+
+tic = time.time()  # capture start time
+
+
+#matrix_creation
+a = np.random.random_sample((2, 2))
+print(f"a shape = {a.shape}, a = {a}")
+
+
+#a = np.arange(6).reshape(3, 2)
+#To arrive at the same 3 row, 2 column array.
+# #The -1 argument tells the routine to compute the number of rows given the size
+# of the array and the number of columns.
+
+
+
+
